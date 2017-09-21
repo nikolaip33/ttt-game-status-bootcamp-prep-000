758bd18c2500ab8635df910e1dc546d92b19cd4b
Nikolai Pawlowski <nikolaip@gmail.com>
2017-09-21 22:06:33 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -4,3 +4,44 @@ def position_taken?(board, index)
 end
 
 # Define your WIN_COMBINATIONS constant
+WIN_COMBINATIONS = [
+    [0,1,2],
+    [3,4,5],
+    [6,7,8],
+    [0,3,6],
+    [1,4,7],
+    [2,5,8],
+    [0,4,8],
+    [2,4,6]
+]
+
+def full?(board)
+  !board.detect{|i| i == " "}
+end
+
+def won?(board)
+
+  var current = []
+  WIN_COMBINATIONS.each do |combination|
+    puts "going!"
+=begin
+    if !postion_taken(board[combination[0]])
+      puts "doing!"
+      current = []
+      combination.each do |i|
+        current << board[i]
+      end
+      token = current[0]
+      if current.all? do |space|
+          space == token
+        end
+        current
+      else
+        false
+      end
+    else
+      false
+    end
+=end
+  end
+end
